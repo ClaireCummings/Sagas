ca2409c946794d534246465bc5cc351314f6aee0
ClaireCummings <claire.cummings@advancedcomputersoftware.com>
2013-10-30 16:58:33 +0000

    Removed commented out code

--- LFMSubmissions/LFM.Submissions.AgentComms/LandRegistry/IEdrsAttachmentSender.cs
+++ LFMSubmissions/LFM.Submissions.AgentComms/LandRegistry/IEdrsAttachmentSender.cs
@@ -5,11 +5,6 @@ namespace LFM.Submissions.AgentComms.LandRegistry
     public interface IEdrsAttachmentSender : IEdrsPoller<IEdrsAttachmentResponseReceived>
     {
         string ApplicationMessageId { get; set; }
-//        string MessageId { get; set; }
-//        string Username { get; set; }
-//        string Password { get; set; }
         string Payload { get; set; }
-//        IEdrsAttachmentResponseReceived Response { get; }
-//        bool Submit();
     }
 }
--- LFMSubmissions/LFM.Submissions.AgentComms/LandRegistry/IEdrsSender.cs
+++ LFMSubmissions/LFM.Submissions.AgentComms/LandRegistry/IEdrsSender.cs
@@ -4,11 +4,6 @@ namespace LFM.Submissions.AgentComms.LandRegistry
 {
     public interface IEdrsSender : IEdrsPoller<IEdrsResponseReceived>
     {
-//        string MessageId { get; set; }
-//        string Username { get; set; }
-//        string Password { get; set; }
         string Payload { get; set; }
-//        IEdrsResponseReceived Response { get;}
-//        bool Submit();
     }
 }
